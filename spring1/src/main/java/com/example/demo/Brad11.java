ff323a76e5debe012889fe171dcb9edc87456fa2
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-28 16:41:08 +0800

    05281641

--- spring1/src/main/java/com/example/demo/Brad11.java
+++ spring1/src/main/java/com/example/demo/Brad11.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,17 +15,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.mapper.OrderDetailRowMapper;
+import com.example.demo.model.Order;
 import com.example.demo.model.OrderDetail;
 
 // 更換資料庫: application.properties => north
 @RestController
 @RequestMapping("/brad11")
 public class Brad11 {
+
+    private final OrderDetail orderDetail;
 	@Autowired
 	private NamedParameterJdbcTemplate jdbc;
 	
 	@Autowired
 	private OrderDetailRowMapper mapper;
+
+    Brad11(OrderDetail orderDetail) {
+        this.orderDetail = orderDetail;
+    }
 	
 	/*
 	 * SELECT o.OrderID id, p.ProductName pname, 
@@ -45,4 +55,47 @@ public class Brad11 {
 		
 		return jdbc.query(sql, params, mapper);
 	}
+	@GetMapping(value = {"/orders", "/orders/{orderId}"})
+	public List<Order> test2(@PathVariable(required = false) Integer orderId){
+		String sql = "SELECT o.OrderID id, o.OrderDate odate, p.ProductName pname, "
+				+ "		od.Quantity qty, od.UnitPrice price "
+				+ "		FROM orders o"
+				+ "		JOIN orderdetails od ON o.OrderID = od.OrderID"
+				+ "		JOIN products p ON od.ProductID = p.ProductID";
+		
+				Map<String,Object> params = new HashMap<>();
+				if (orderId != null) {
+					sql += "		WHERE o.OrderID = :orderId";
+					params.put("orderId", orderId);
+				}
+				HashMap<Integer, Order> orders = new HashMap<>();
+				
+				List<Map<String,Object>> orderMap = jdbc.queryForList(sql, params);
+				for (Map<String,Object> row: orderMap) {
+					int oid = (Integer)row.get("id");
+					
+					Order order = orders.get(oid);
+					if (order == null) {
+						order = new Order();
+						order.setOrderId(oid);
+						LocalDateTime odate = (LocalDateTime)row.get("odate");
+						order.setOrderDate(odate.toString());
+					}
+					
+					OrderDetail detail = new OrderDetail();
+					detail.setUnitPrice(((BigDecimal)row.get("price")).doubleValue());
+					detail.setProductName((String)row.get("pname"));
+					detail.setQty((Integer)row.get("qty"));
+					
+					order.getOrderDetails().add(detail);
+				}
+		
+		
+		
+		return new ArrayList<Order>(orders.values());
+	}
+	
+	
+	
+	
 }
