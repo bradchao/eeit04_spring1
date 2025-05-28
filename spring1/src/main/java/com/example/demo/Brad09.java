0d701aa7f604518398ad1b5c88738249c4fb6f15
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-28 12:01:34 +0800

    05281201

--- spring1/src/main/java/com/example/demo/Brad09.java
+++ spring1/src/main/java/com/example/demo/Brad09.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +18,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RestController
 @RequestMapping("/brad09")
 public class Brad09 {
+
+	@Autowired
+	private NamedParameterJdbcTemplate jdbc;
 	
 	@RequestMapping("/test1")
 	public void test1() {
@@ -31,8 +38,26 @@ public class Brad09 {
 //				System.out.println(hotel.getName());
 //			}
 			
+			jdbc.update("DELETE FROM hotel", new MapSqlParameterSource());
+			jdbc.update("ALTER TABLE hotel auto_increment = 1", 
+					new MapSqlParameterSource());
 			
+			String sql = "INSERT INTO hotel " + 
+					"(name,addr,tel) " + 
+					"VALUES (:name, :addr, :tel)";
+			KeyHolder keyHolder = new GeneratedKeyHolder();
+			MapSqlParameterSource[] params = 
+				new MapSqlParameterSource[hotels.size()];
+			for (int i=0; i<hotels.size(); i++) {
+				Hotel hotel = hotels.get(i);
+				params[i] = new MapSqlParameterSource();
+				params[i].addValue("name", hotel.getName());
+				params[i].addValue("addr", hotel.getAddr());				
+				params[i].addValue("tel", hotel.getTel());				
+			}
 			
+			jdbc.batchUpdate(sql, params, keyHolder);			
+			System.out.println("Finish");
 		} catch (Exception e) {
 			System.out.println(e);
 		}
