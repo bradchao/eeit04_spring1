e78d4cf8c51471c53eb1001c84b0c6cba73c9bb4
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-28 11:22:19 +0800

    05281122

---
+++ spring1/src/main/java/com/example/demo/Brad09.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.model.Hotel;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping("/brad09")
+public class Brad09 {
+	
+	@RequestMapping("/test1")
+	public void test1() {
+		String url = 
+			"https://data.moa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelStay.aspx";
+		RestTemplate template = new RestTemplate();
+		String content = template.getForObject(url, String.class);
+		//System.out.println(content);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			List<Hotel> hotels =
+				mapper.readValue(content, new TypeReference<List<Hotel>>() {});
+			for (Hotel hotel :hotels) {
+				System.out.println(hotel.getName());
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
