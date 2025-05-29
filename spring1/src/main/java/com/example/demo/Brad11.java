1a269b1f11bd7b97c1bfe409bea33a211852524a
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-29 10:08:54 +0800

    05291008

--- spring1/src/main/java/com/example/demo/Brad11.java
+++ spring1/src/main/java/com/example/demo/Brad11.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,8 +25,15 @@ import com.example.demo.model.OrderDetail;
 public class Brad11 {
 
     private final OrderDetail orderDetail;
-	@Autowired
-	private NamedParameterJdbcTemplate jdbc;
+    
+//	@Autowired
+//	private NamedParameterJdbcTemplate jdbc;
+    
+    @Autowired
+    @Qualifier("northJdbcTemplete")
+    private NamedParameterJdbcTemplate jdbc;
+    
+    
 	
 	@Autowired
 	private OrderDetailRowMapper mapper;
